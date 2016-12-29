d1e775c0b87510c79c776ca90baa92c85d2e9ce8
Lorna Jane Mitchell <lorna@lornajane.net>
2016-12-29 20:42:44 +0000

    First working Alexa skill - fetches bus times

---
+++ src/index.js
@@ -0,0 +1,190 @@
+// original code from https://medium.com/@bthdonohue/build-your-first-alexa-skill-8a37dc3103d6#.rib1s6mb8
+
+'use strict';
+var http = require('http');
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var cardTitle = "Hello, World!"
+    var speechOutput = "You can tell Hello, World! to say Hello, World!"
+    callback(session.attributes,
+        buildSpeechletResponse(cardTitle, speechOutput, "", true));
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == 'TestIntent') {
+        handleTestRequest(intent, session, callback);
+    }
+    else if(intentName == 'Ping') {
+        handlePingRequest(intent, session, callback);
+    }
+    else if(intentName == 'Bus') {
+        handleBusRequest(intent, session, callback);
+    }
+    else {
+        // just do the test thing now to see things succeed
+        handleTestRequest(intent, session, callback);
+
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+}
+
+function handleTestRequest(intent, session, callback) {
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Hello, World!", "", "true"));
+}
+
+function handlePingRequest(intent, session, callback) {
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Ping ack", "", "true"));
+}
+
+function handleBusRequest(intent, session, callback) {
+    // put your own URL here
+    var url = 'http://example.com';
+
+    var body = '';
+    var buses = [];
+    var speak = '';
+    var req = http.get(url, function (res) {
+        res.on('data', function(data) {
+            body = body + data;
+        });
+
+        res.on('end', function() {
+            var regex = /<td class="body-cell">(.*)<\/td><td class="body-cell">(.*)<\/td><td class="body-cell" align="right">(.*)<\/td>.*<\/td>/g;
+
+            var looping = 1;
+            while(looping) {
+                var matches = regex.exec(body);
+                if(Array.isArray(matches)) {
+                    buses.push(matches);
+                } else {
+                    looping = 0;
+                }
+            }
+            
+            var i = 0;
+            buses.forEach(function (bus) {
+                if(i < 7) {
+                    speak = speak + "There's a " + bus[1] + " about " + bus[3] + ". ";
+                }
+                i = i + 1;
+            });
+           callback(session.attributes,
+                buildSpeechletResponseWithoutCard("The next bus is soon! " + speak, "", "true"));            
+        });
+    });
+}
+
+// ------- Helper functions to build responses -------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
